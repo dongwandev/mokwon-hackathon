3ab67cbdb3ef0c9d9c489b2a5f581b5692e36b06
Vactor0911 <vactor0911@gmail.com>
2025-09-28 02:36:47 +0900

    홈 페이지 스타일 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import LevelPage from "./pages/LevelPage.jsx";
 import LevelResultPage from "./pages/LevelResultPage.jsx";
 import LearningChoicePage from "./pages/LearningChoicePage.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import './App.css'
 import ProblemPage from "./pages/ProblemPage.jsx";
 import LearningPage from "./pages/LearningPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 function Home() {
   return (
@@ -27,12 +27,19 @@ function App() {
         <Route path="/learningchoice" element={<LearningChoicePage />} />
         <Route path="/home" element={<HomePage />} />
         {/* 선택: 404 최소 대응 */}
-        <Route path="*" element={<div style={{ padding: 16 }}>Not Found</div>} />
-        <Route path="/LearningPage" element={<LearningPage/>}/>
-        <Route path="/ProblemPage" element={<ProblemPage/>}/>
+        <Route
+          path="*"
+          element={<div style={{ padding: 16 }}>Not Found</div>}
+        />
+        <Route path="/LearningPage" element={<LearningPage />} />
+        <Route path="/ProblemPage" element={<ProblemPage />} />
       </Routes>
+
+      {/* 푸터 */}
+      <Footer />
+      
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
