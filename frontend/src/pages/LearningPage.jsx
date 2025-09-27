4546412f66fbe8e87bba5a8c2f0f2a93cc91ae17
ns-02 <lemen267@gmail.com>
2025-09-28 03:57:27 +0900

    로딩 페이지, 학습 페이지 CSS 수정

--- frontend/src/pages/LearningPage.jsx
+++ frontend/src/pages/LearningPage.jsx
@@ -1,19 +1,18 @@
+import Container from "../components/Container"
 import "./pages.css"
 
 const LearningPage = () => {
   return (
-    <div>
+    <Container>
       <h2>학습 페이지</h2>
 
-      {/* 거대한 컨테이너로 감싸기 */}
       <div className="big-container">
         <p>학습 내용입니다.</p>
       </div>
       <div>
         <button>다음 페이지</button>
       </div>
-      
-    </div>
+    </Container>
   )
 }
 
