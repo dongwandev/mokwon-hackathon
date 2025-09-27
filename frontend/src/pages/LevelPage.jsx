d931c37b35e57a8284c17b6b2779b19e708d8063
ns-02 <lemen267@gmail.com>
2025-09-27 20:59:58 +0900

    App.jsx 라우터 설정 및 LevelPage.jsx 변경

---
+++ frontend/src/pages/LevelPage.jsx
@@ -0,0 +1,9 @@
+function LevelPage() {
+    return (
+        <main style={{ padding: 16 }}>
+            <h1>Level</h1>
+            <p>여기는 /level 페이지</p>
+        </main>
+    );
+}
+export default LevelPage;
